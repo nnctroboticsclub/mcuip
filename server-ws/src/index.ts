d4681ec2365ca3189cc37eabe00788cd0ffff163
syoch <syoch64@gmail.com>
2023-11-04 09:11:45 +0900

    Update: test writing Bus.serve()

--- server-ws/src/index.ts
+++ server-ws/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Under construction...')
+Bun.serve({
+  port: 3000,
+  websocket: {
+    open: (ws) => {
+      console.log(`New client connected: ${ws.remoteAddress}`);
+    },
+    close: (ws) => {
+      console.log(`Client disconnected: ${ws.remoteAddress}`);
+    },
+    message: (ws, message) => {
+      console.log(`Message from ${ws.remoteAddress}: ${message}`);
+    }
+  },
+  fetch: (req, server) => {
+    console.log(`Request: ${req.method} ${req.url}`);
+
+    if (server.upgrade(req)) {
+      return;
+    }
+
+    return new Response("The supports only WebSocket.", {
+      status: 400,
+      statusText: "Bad Request"
+    });
+  }
+})
