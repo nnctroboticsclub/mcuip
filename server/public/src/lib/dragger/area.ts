fd40ebf5037cf92d24b227580da427e5e5350915
syoch <syoch64@gmail.com>
2023-11-22 19:40:27 +0900

    Update: Impl fitInArea

--- server/public/src/lib/dragger/area.ts
+++ server/public/src/lib/dragger/area.ts
@@ -7,6 +7,23 @@ export class Area {
     return `top: ${this.top_}px; left: ${this.left_}px; width: ${this.width_}px; height: ${this.height_}px;`;
   }
 
+  getPosition(): Position {
+    return new Position(this.left_, this.top_);
+  }
+
+  sizeVector(): Position {
+    return new Position(this.width_, this.height_);
+  }
+
+  moveTo(pos: Position): Area {
+    const { x, y } = pos.components();
+    return new Area(y, x, this.width_, this.height_);
+  }
+
+  relativeArea(): Area {
+    return new Area(0, 0, this.width_, this.height_);
+  }
+
   fitToArea(pos: Position): Position {
     const tl = new Position(this.left_, this.top_);
     const br = new Position(this.left_ + this.width_, this.top_ + this.height_);
@@ -14,5 +31,18 @@ export class Area {
     console.log(`fitToArea: ${pos.toString()} -> ${new_pos.toString()} (tl: ${tl.toString()}, br: ${br.toString()})`);
     console.log(`-> top: ${this.top_}, left: ${this.left_}, width: ${this.width_}, height: ${this.height_}`)
     return new_pos;
+  }
+
+  fitInArea(other: Area): Area | null {
+    if (this.width_ > other.width_ || this.height_ > other.height_) {
+      return null;
+    }
+
+    return other.moveTo(
+      this
+        .getPosition()
+        .subtract(this.sizeVector())
+        .min(other.getPosition())
+    )
   }
 }
