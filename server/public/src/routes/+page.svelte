28f693f91bb7b22e57d5eb41946c14dedae4bbbc
syoch <syoch64@gmail.com>
2023-11-22 12:06:40 +0900

    Update: Edit top page

--- server/public/src/routes/+page.svelte
+++ server/public/src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
   import WindowWrapper from "$lib/apps/window_wrapper.svelte";
   import { windows } from "$lib/window/windows";
+
+  import DraggableArea from "$lib/dragger/area.svelte";
+
+  let width: number, height: number;
 </script>
 
-{#each windows as window}
-  <WindowWrapper {window} />
-{/each}
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<DraggableArea top={0} left={0} width={width / 2} height={height / 2}>
+  <!-- {#each windows as window}
+    <WindowWrapper {window} />
+  {/each} -->
+</DraggableArea>
