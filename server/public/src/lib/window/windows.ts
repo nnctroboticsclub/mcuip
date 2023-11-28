89554dd81db2849494ac430fb687f3a60404de82
syoch <syoch64@gmail.com>
2023-11-28 17:59:53 +0900

    Update: Support Launch new app

--- server/public/src/lib/window/windows.ts
+++ server/public/src/lib/window/windows.ts
@@ -1,10 +1,29 @@
-import { writable, type Writable } from "svelte/store";
-import type { WindowConfig } from "./window";
+import { get, writable, type Writable } from "svelte/store";
+import { WindowConfig } from "./window";
 import { getContext, setContext } from "svelte";
+import { Area } from "$lib/ui/area";
+
+class WindowPositionCalculator {
+  private i = 0;
+  constructor(private windows: Writable<WindowConfig[]>) { }
+
+  GeneratePosition() {
+    const x = 10 + 20 * this.i;
+    const y = 10 + 20 * this.i;
+    this.i++;
+    return { x, y };
+
+  }
+}
 
 export class WindowManagerContext {
+  private position_calculator: WindowPositionCalculator;
+
   private windows_: Writable<WindowConfig[]> = writable([]);
 
+  constructor() {
+    this.position_calculator = new WindowPositionCalculator(this.windows_);
+  }
 
   get windows(): Writable<WindowConfig[]> {
     return this.windows_;
@@ -24,7 +43,18 @@ export class WindowManagerContext {
   }
 
   launch(app_name: string, window_data: Writable<any> = writable({})) {
-    console.log(`Launching ${app_name} with data:`, window_data);
+    const { x, y } = this.position_calculator.GeneratePosition();
+    const area = new Area(x, y, 400, 400);
+    console.log("launching window");
+    console.log(`  - app_name: ${app_name}`);
+    console.log(`  - window_data: ${get(window_data)}`);
+    console.log(`  - area: ${area}`);
+    const window = new WindowConfig(
+      writable(area),
+      app_name,
+      window_data
+    );
+    this.addWindow(window);
     return window;
   }
 
