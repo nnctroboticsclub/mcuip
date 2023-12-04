5c917552f6d1a88bc89aff2d7708aa17f32aea73
syoch <syoch64@gmail.com>
2023-12-04 23:24:35 +0900

    Update: Added state visualizer

--- server/public/src/lib/apps/global_data_list.svelte
+++ server/public/src/lib/apps/global_data_list.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-  import { writable } from "svelte/store";
-  import { global_state } from "../../global_state";
   import Window from "../window/window.svelte";
-  import DebuggerTree from "./debugger_tree.svelte";
   import { shared_data } from "$lib/shared_data/global";
-
-  export let object: { [key: string]: any } = global_state;
+  import GlobalDataListPart from "./global_data_list_part.svelte";
 
   const store = shared_data.dataListStore();
 </script>
@@ -14,7 +10,7 @@
   <svelte:fragment slot="title">Global Data List</svelte:fragment>
   <svelte:fragment slot="app">
     {#each $store as item}
-      {item}
+      <GlobalDataListPart key={item.key} value={item.value} />
     {/each}
   </svelte:fragment>
 </Window>
---
+++ server/public/src/lib/apps/global_data_list_part.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import type { Writable } from "svelte/store";
+
+  export let key: string;
+  export let value: Writable<string>;
+</script>
+
+<span>{key} {$value}</span>
