6f3867ddf9c6ec5b1f872dce166f52bcca040c9e
syoch <syoch64@gmail.com>
2024-01-02 15:49:53 +0900

    Update: Use 2023KoroboNC with fullscreen

--- server/public/src/routes/+page.svelte
+++ server/public/src/routes/+page.svelte
@@ -3,6 +3,9 @@
   import { Area } from "$lib/ui/area";
   import Windows from "$lib/window/windows.svelte";
   import { patch_console_log } from "$lib/console_patch";
+  import { WindowConfig, setWindow } from "$lib/window/window";
+  import { writable } from "svelte/store";
+  import Korobo2023ncMain from "$lib/apps/korobo-2023-nc/korobo2023nc-main.svelte";
 
   let width: number, height: number;
 
@@ -14,10 +17,20 @@
   // window_manager.addWindow(new Area(130, 0, 600, 470), "mcuip-ctrl", {});
   // window_manager.addWindow(new Area(0, 620, 300, 600), "window_inspector", {});
   window_manager.addWindow(new Area(0, 0, 1000, 330), "2023-korobo-nc", {});
+
+  setWindow(
+    new WindowConfig(
+      writable(new Area(0, 0, 0, 0)),
+      "2023-korobo-nc",
+      {},
+      writable(0)
+    )
+  );
 </script>
 
 <div class="container" bind:clientWidth={width} bind:clientHeight={height}>
-  <Windows {width} {height} />
+  <!-- <Windows {width} {height} /> -->
+  <Korobo2023ncMain></Korobo2023ncMain>
 </div>
 
 <style>
