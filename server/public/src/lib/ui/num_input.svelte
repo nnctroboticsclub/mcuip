38de07e752910f87e170dbe1881ab11f9ec01df0
syoch <syoch64@gmail.com>
2024-02-24 04:39:52 +0900

    Update: Added NumInput

---
+++ server/public/src/lib/ui/num_input.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import TextInput from "./text_input.svelte";
+
+  export let value: number = 0;
+  export let width: string = "100%";
+  export let height: string = "100%";
+  export let line_height: string = "1em";
+
+  let value_text = value.toString();
+  $: value = parseFloat(value_text);
+</script>
+
+<TextInput bind:value={value_text} {width} {height} {line_height} />
